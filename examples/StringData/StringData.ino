dea093475715bf69bf1fb54fdc67ff479843392a
WifWaf <43455052+WifWaf@users.noreply.github.com>
2020-01-04 21:51:37 +0000

    Update StringData.ino

--- examples/StringData/StringData.ino
+++ examples/StringData/StringData.ino
@@ -3,8 +3,8 @@
 
 AT24CM01 EEPROM;            // Library 
 
-#define TXTLEN 6            // String length
-char textarray[TXTLEN];     // text buffer
+#define AR_LEN 6            // Array length
+char textarray[AR_LEN];     // text buffer
 String text = "Hello";      // string to be stored
 
 void setup() 
@@ -15,15 +15,15 @@ void setup()
 
   Serial.print("\n.................... Recalling String .................... \n");
   Serial.print("\n Writing << "); Serial.print(text);
-  text.toCharArray(textarray, (TXTLEN));               // Use arduino to move String to text array
-  EEPROM.writeCharArray(20, textarray, TXTLEN-1);      // EEPROM.writeArrayType(address, array, arraysize)  
+  text.toCharArray(textarray, (AR_LEN));               // Use arduino to move String to text array
+  EEPROM.writeCharArray(20, textarray, AR_LEN-1);      // EEPROM.writeArrayType(address, array, arraysize)  
   
-  memset(textarray, 0, TXTLEN-1);                      // Clear text array
+  memset(textarray, 0, AR_LEN-1);                      // Clear text array
   text = "";                                           // Clear String
 
-  EEPROM.readCharArray(20, textarray, TXTLEN-1);       // ReadArrayType(address, textarraybuffer, textsize)
+  EEPROM.readCharArray(20, textarray, AR_LEN-1);       // ReadArrayType(address, textarraybuffer, textsize)
   Serial.print("\n Recieved >> ");
-  for(uint8_t x = 0; x < TXTLEN-1; x++)
+  for(uint8_t x = 0; x < AR_LEN-1; x++)
     text += textarray[x];                              // Add each array value to the string.
 
   Serial.print(text);                                  // Print string
