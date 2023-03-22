abb26aa7d5394c7ad47fef7dfa0f76f55ddc0630
WifWaf <JDWifWaf@gmail.com>
2023-03-22 17:34:54 +0000

    1.1.0
    Added multiple device ID support via either the begin() or during runtime with setDeviceAddress().
    
    Added documentation for non-inline functions by Mintlify.

--- src/AT24CM01.cpp
+++ src/AT24CM01.cpp
@@ -1,141 +1,189 @@
-/*************************************************** 
-  Arduino library for the AT24CM01
-  
+/*
   Author: Jonathan Dempsey JDWifWaf@gmail.com
-  
-  Version: 1.0.1
+  Version: 1.1.0
   License: Apache 2.0
- ****************************************************/
+*/
 
 #include <Wire.h>
 #include "AT24CM01.h"
 
 #define DEBUG 0
-           
-#define GETADR(MGADR) ((MGADR >> 16) | ATDEVADR)
-#define CHCKSIZE(x) ((x)>8 || (x)==0) ? Serial.print("\n<AT24CM01> Error - Size given too large or small (1-8)") : 0
-#define CHCKADDR(x) ((x)>ATMAXADR) ? Serial.print("\n<AT24CM01> Error - Address given too large (0 - 131072)") : 0
 
-/*
-#define AT24CM01_ADR_A1H 0x08
-#define AT24CM01_ADR_A2H 0x14
-#define AT24CM01_ADR_A1L 0x00
-#define AT24CM01_ADR_A2L 0x00            // Not needed unless you want to determine address manually and shift the bytes
+#define GETADR(MGADR, DEVADR) ((MGADR >> 16) | DEVADR)
+#define CHCKSIZE(x) ((x) > 8 || (x) == 0) ? Serial.print("\n<AT24CM01> Error - Size given too large or small (1-8)") : 0
+#define CHCKADDR(x) ((x) > ATMAXADR) ? Serial.print("\n<AT24CM01> Error - Address given too large (0 - 131072)") : 0
 
-#define AT24CM01_PAGE_ADR0 0x50
-#define AT24CM01_PAGE_ADR1 0x51
-#define AT24CM01_WPAGE_ADR0 0x00
-#define AT24CM01_WPAGE_ADR1 0x02
-#define AT24CM01_WORD_ADR 0xA0
-*/
-
-void AT24CM01::begin(TwoWire &inWire) 
+/**
+ * This function sets up the AT24CM01 EEPROM for use with the Arduino.
+ *
+ * @param inWire The TwoWire object you want to use.
+ * @param device_adr The address of the device on the I2C bus.
+ */
+void AT24CM01::begin(TwoWire &inWire, uint8_t device_adr)
 {
   this->ATWire = &inWire;
   this->ATWire->begin();
   this->ATWire->setClock(AT2CSPEED);
+  this->ATDevAdr = device_adr;
 }
 
+/**
+ * This function sets the device address of the AT24CM01 EEPROM
+ *
+ * @param device_adr The address of the device.
+ */
+void AT24CM01::setDeviceAddress(uint8_t device_adr)
+{
+  this->ATDevAdr = device_adr;
+}
+
+/**
+ * Reads a 32-bit value from the specified address
+ *
+ * @param address The address of the memory location you want to read from.
+ * @param size The number of bytes to read.
+ *
+ * @return The data read from the EEPROM.
+ */
 uint32_t AT24CM01::read(uint32_t address, uint8_t size)
 {
-  if(CHCKADDR(address) || CHCKSIZE(size))
+  if (CHCKADDR(address) || CHCKSIZE(size))
     return 0;
- 
-  this->ATWire->beginTransmission((uint8_t)GETADR(address));
-  this->ATWire->write((uint8_t)((address & 0xFFFF) >> 8));  
+
+  this->ATWire->beginTransmission((uint8_t)GETADR(address, ATDevAdr));
+  this->ATWire->write((uint8_t)((address & 0xFFFF) >> 8));
   this->ATWire->write((uint8_t)(address & 0xFF));
   this->ATWire->endTransmission();
 
-  this->ATWire->requestFrom((uint8_t)GETADR(address), (uint8_t)size); 
+  this->ATWire->requestFrom((uint8_t)GETADR(address, ATDevAdr), (uint8_t)size);
   uint32_t buff0 = 0;
-  uint32_t buff1 = 0; 
-  if(this->ATWire->available())
-  { 
-    for(uint8_t x = 0; x < size; x++)
+  uint32_t buff1 = 0;
+  if (this->ATWire->available())
+  {
+    for (uint8_t x = 0; x < size; x++)
     {
       buff1 = this->ATWire->read();
-      if(!buff1)
+      if (!buff1)
         continue;
-      buff0 |= (buff1 << x*8);
+      buff0 |= (buff1 << x * 8);
     }
   }
   return buff0;
 }
 
+/**
+ * The function reads a specified number of bytes from the specified address and stores them in the
+ * specified array
+ *
+ * @param address The address of the first byte to read.
+ * @param data The array of data to be written to the EEPROM.
+ * @param indexCount The number of bytes to read.
+ *
+ * @return The data read from the EEPROM.
+ */
 void AT24CM01::read(uint32_t address, uint8_t data[], uint8_t indexCount)
 {
-  if(CHCKADDR(address))
+  if (CHCKADDR(address))
     return;
 
   memset(data, 0, indexCount);
- 
-  this->ATWire->beginTransmission((uint8_t)GETADR(address));
+
+  this->ATWire->beginTransmission((uint8_t)GETADR(address, ATDevAdr));
   this->ATWire->write((uint8_t)((address & 0xFFFF) >> 8));
   this->ATWire->write((uint8_t)(address & 0xFF));
   this->ATWire->endTransmission();
 
-  this->ATWire->requestFrom((uint8_t)GETADR(address), indexCount); 
-  if(this->ATWire->available())
-  { 
-    for(uint8_t x = 0; x < indexCount; x++)
+  this->ATWire->requestFrom((uint8_t)GETADR(address, ATDevAdr), indexCount);
+  if (this->ATWire->available())
+  {
+    for (uint8_t x = 0; x < indexCount; x++)
       data[x] = this->ATWire->read();
   }
 }
 
+/**
+ * The function takes an address and data, and writes the data to the address
+ *
+ * @param address The address of the memory location to write to.
+ * @param data The data to be written to the EEPROM.
+ *
+ * @return a boolean value.
+ */
 void AT24CM01::write(uint32_t address, uint32_t data)
 {
-  if(CHCKADDR(address))
+  if (CHCKADDR(address))
     return;
 
-  this->ATWire->beginTransmission((uint8_t)GETADR(address));                // Start, and determine segment address 
-  this->ATWire->write((uint8_t)((address & 0xFF00) >> 8));                  // Set curser to address for writing
+  this->ATWire->beginTransmission((uint8_t)GETADR(address, ATDevAdr)); // Start, and determine segment address
+  this->ATWire->write((uint8_t)((address & 0xFF00) >> 8));             // Set curser to address for writing
   this->ATWire->write((uint8_t)(address & 0xFF));
-  
+
   uint8_t sBuff = 0;
-  for(uint8_t x = 0; (data & (0xFFFFFFFF << x*8)); x++)                // Determine data size
-  {                                                                    
-    sBuff = (data >> x*8) & 0xFF;                                      // Value if required into appropriate parts
-    this->ATWire->write(sBuff);                                              // Address rolls over when stop is not called - see datasheet.
+  for (uint8_t x = 0; (data & (0xFFFFFFFF << x * 8)); x++) // Determine data size
+  {
+    sBuff = (data >> x * 8) & 0xFF; // Value if required into appropriate parts
+    this->ATWire->write(sBuff);     // Address rolls over when stop is not called - see datasheet.
   }
-  
-  this->ATWire->endTransmission();                                           // Stop
-  
-  delay(ATWDelay);                                                     // Delay required to stop write/read failing if carried out abruptly
+
+  this->ATWire->endTransmission(); // Stop
+
+  delay(ATWDelay); // Delay required to stop write/read failing if carried out abruptly
 }
+
+/**
+ * The function takes an address, data and size as parameters. It then splits the data into bytes and
+ * writes them to the address
+ *
+ * @param address The address of the memory location to write to.
+ * @param data The data to be written to the EEPROM
+ * @param size The number of bytes to write.
+ *
+ * @return a boolean value.
+ */
 
 void AT24CM01::write(uint32_t address, uint32_t data, uint8_t size)
 {
-  if(CHCKADDR(address) || CHCKSIZE(size))
+  if (CHCKADDR(address) || CHCKSIZE(size))
     return;
-  this->ATWire->beginTransmission((uint8_t)GETADR(address));                 // Start, and determine segment address 
-  this->ATWire->write((uint8_t)((address & 0xFF00) >> 8));                   // Set curser to address for writing
+  this->ATWire->beginTransmission((uint8_t)GETADR(address, ATDevAdr)); // Start, and determine segment address
+  this->ATWire->write((uint8_t)((address & 0xFF00) >> 8));             // Set curser to address for writing
   this->ATWire->write((uint8_t)(address & 0xFF));
-  
+
   uint8_t sBuff = 0;
-  for(uint8_t x = 0; x < size; x++)                                      
-  {                                                                   
-    sBuff = (data >> x*8) & 0xFF;                                            // Split into bytes and write
-    this->ATWire->write(sBuff);                                              // Address rolls over when stop is not called - see datasheet;
+  for (uint8_t x = 0; x < size; x++)
+  {
+    sBuff = (data >> x * 8) & 0xFF; // Split into bytes and write
+    this->ATWire->write(sBuff);     // Address rolls over when stop is not called - see datasheet;
   }
-  
-  this->ATWire->endTransmission();                                           // Stop
-  
-  delay(ATWDelay);                                                           // Delay required to stop write/read failing if carried out abruptly
+
+  this->ATWire->endTransmission(); // Stop
+
+  delay(ATWDelay); // Delay required to stop write/read failing if carried out abruptly
 }
+
+/**
+ * This function writes a byte array to the specified address
+ *
+ * @param address The address of the first byte to write to.
+ * @param data The data to be written to the EEPROM.
+ * @param indexCount The number of bytes to write.
+ *
+ * @return Nothing.
+ */
 
 void AT24CM01::write(uint32_t address, uint8_t data[], uint8_t indexCount)
 {
-  if(CHCKADDR(address))
+  if (CHCKADDR(address))
     return;
-  
-  this->ATWire->beginTransmission((uint8_t)GETADR(address));
+
+  this->ATWire->beginTransmission((uint8_t)GETADR(address, ATDevAdr));
   this->ATWire->write((uint8_t)((address & 0xFF00) >> 8));
   this->ATWire->write((uint8_t)(address & 0xFF));
-  
-  for(uint8_t x = 0; x < indexCount; x++)
+
+  for (uint8_t x = 0; x < indexCount; x++)
     this->ATWire->write(data[x]);
 
   this->ATWire->endTransmission();
-  
+
   delay(ATWDelay);
 }
--- src/AT24CM01.h
+++ src/AT24CM01.h
@@ -1,11 +1,8 @@
-/*************************************************** 
-  Arduino library for the AT24CM01
-  
+/*
   Author: Jonathan Dempsey JDWifWaf@gmail.com
-  
   Version: 1.0.1
   License: Apache 2.0
- ****************************************************/
+*/
 
 #ifndef _AT24CM01_H
 #define _AT24CM01_H
@@ -13,125 +10,161 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-#define ATWDelay 5                   // Delay to allow write to be completed (datasheet shows max 5ms)
-#define AT2CSPEED 400000             // Clock speed for I2C
-#define ATMAXADR 131072              // Size of EEPROM in words
-#define ATDEVADR 0x50                // Lowest page address
+#define ATWDelay 5       // Delay to allow write to be completed (datasheet shows max 5ms)
+#define AT2CSPEED 400000 // Clock speed for I2C
+#define ATMAXADR 131072  // Size of EEPROM in words
+
+#define ATDEVADR0 0x50 // Lowest page address (default initialiser value) for Address 0
+#define ATDEVADR1 0x52 // Lowest page address (default initialiser value) for Address 1
+#define ATDEVADR2 0x54 // Lowest page address (default initialiser value) for Address 2
+#define ATDEVADR3 0x56 // Lowest page address (default initialiser value) for Address 3
 
 class AT24CM01
 {
-    public:
-        void begin(TwoWire &inWire = Wire);
+public:
+    void begin(TwoWire &inWire = Wire, uint8_t device_adr = ATDEVADR0);
 
-        uint32_t read(uint32_t address, uint8_t size = 1);
-        void read(uint32_t address, uint8_t data[], uint8_t indexCount);
-        
-        void write(uint32_t address, uint32_t data);
-        void write(uint32_t address, uint32_t data, uint8_t size);
-        void write(uint32_t address, uint8_t data[], uint8_t indexCount);
-        
-        /* Variable Specific Functions conversions ................................................ */
+    uint32_t read(uint32_t address, uint8_t size = 1);
+    void read(uint32_t address, uint8_t data[], uint8_t indexCount);
 
-        /* ------------------------ Write --------------------- */
-        inline void writeBool(uint32_t a, bool b) {
-            write((uint32_t)a, (uint32_t)b, sizeof(bool));
-        }
-        inline void writeCharArray(uint32_t a, char c[], uint8_t i) {
-            write((uint32_t)a, (uint8_t*)c, (uint8_t)i);
-        }
-        inline void writeByteArray(uint32_t a, byte y[], uint8_t i) {
-            write((uint32_t)a, (uint8_t*)y, (uint8_t)i);
-        }
-        inline void writeByte(uint32_t a, byte y) {
-            write((uint32_t)a, (uint32_t)y, sizeof(byte)); 
-        }
-        inline void writeChar(uint32_t a, char c) { 
-            write((uint32_t)a, (uint32_t)c, sizeof(char));
-        }         
-        inline void writeUnsignedChar(uint32_t a, unsigned char uc) { 
-            write((uint32_t)a, (uint32_t)uc, sizeof(unsigned char));
-        } 
-        inline void writeShort(uint32_t a, short l) { 
-            write((uint32_t)a, (uint32_t)l, sizeof(short));
-        }
-        inline void writeLong(uint32_t a, long l) { 
-            write((uint32_t)a, (uint32_t)l, sizeof(long));
-        }
-        inline void writeUnsignedLong(uint32_t a, unsigned long ul) { 
-            write((uint32_t)a, (uint32_t)ul, sizeof(unsigned long)); 
-        }
-        inline void writeInt(uint32_t a, unsigned int i) {
-            write((uint32_t)a, (uint32_t)i, sizeof(unsigned int));
-        }
-        inline void writeUnsignedInt(uint32_t a, unsigned int ui) {
-            return write((uint32_t)a, (uint32_t)ui, sizeof(unsigned int));
-        }
-        inline void writeWord(uint32_t a, word ui) {
-            return write((uint32_t)a, (uint32_t)ui, sizeof(word));
-        }
-        inline void writeDouble(uint32_t a, double d) {
-            mkconv.dval = d;
-            write((uint32_t)a, (uint8_t*)mkconv.bval, sizeof(double));
-        }
-        inline void writeFloat(uint32_t a, float f) {
-            mkconv.fval = f;
-            write((uint32_t)a, (uint8_t*)mkconv.bval, sizeof(float));
-        }
-       /* ------------------------ Read --------------------- */
+    void write(uint32_t address, uint32_t data);
+    void write(uint32_t address, uint32_t data, uint8_t size);
+    void write(uint32_t address, uint8_t data[], uint8_t indexCount);
 
-        inline bool readBool(uint32_t a) {
-            return (bool)read((uint32_t)a, sizeof(bool));
-        }
-        inline void readCharArray(uint32_t a, char c[], uint8_t i) {
-            read((uint32_t)a, (uint8_t*)c, (uint8_t)i); 
-        }
-        inline void readByteArray(uint32_t a, uint8_t y[], uint8_t i) {
-            read((uint32_t)a, (uint8_t*)y, (uint8_t)i);
-        }
-        inline byte readByte(uint32_t a) {
-            return (byte)read((uint32_t)a, sizeof(byte)); 
-        }
-        inline char readChar(uint32_t a) {
-            return (char)read((uint32_t)a, sizeof(char));
-        }
-        inline unsigned char readUnsignedChar(uint32_t a) { 
-            return (unsigned char)read((uint32_t)a, sizeof(unsigned char));
-        } 
-        inline short readShort(uint32_t a) { 
-            return (short)read((uint32_t)a, sizeof(short));
-        }
-        inline long readLong(uint32_t a) { 
-            return (long)read((uint32_t)a, sizeof(long));
-        }
-        inline unsigned long readUnsignedLong(uint32_t a) { 
-            return (unsigned long)read((uint32_t)a, sizeof(unsigned long)); 
-        }
-        inline int readInt(uint32_t a) {
-            return (int)read((uint32_t)a, sizeof(unsigned int));
-        }
-        inline unsigned int readUnsignedInt(uint32_t a) {
-            return (unsigned int)read((uint32_t)a, sizeof(unsigned int));
-        }
-        inline word readword(uint32_t a) {
-            return (word)read((uint32_t)a, sizeof(word));
-        }
-        inline double readDouble(uint32_t a) {
-            read((uint32_t)a, (uint8_t*)mkconv.bval, sizeof(double));
-            return mkconv.dval;
-        }
-        inline float readFloat(uint32_t a) {
-            read((uint32_t)a, (uint8_t*)mkconv.bval, sizeof(float));
-            return mkconv.fval;
-        }
+    void setDeviceAddress(uint8_t address);
 
-    protected:
-    private:
-        TwoWire* ATWire;             // TwoWire library isntance
-        union conv {
-                float fval;
-                double dval;
-                uint8_t bval[sizeof(double)];
-        } mkconv;
+    /* Variable Specific Functions conversions ................................................ */
+
+    /* ------------------------ Write --------------------- */
+    inline void writeBool(uint32_t a, bool b)
+    {
+        write((uint32_t)a, (uint32_t)b, sizeof(bool));
+    }
+    inline void writeCharArray(uint32_t a, char c[], uint8_t i)
+    {
+        write((uint32_t)a, (uint8_t *)c, (uint8_t)i);
+    }
+    inline void writeByteArray(uint32_t a, byte y[], uint8_t i)
+    {
+        write((uint32_t)a, (uint8_t *)y, (uint8_t)i);
+    }
+    inline void writeByte(uint32_t a, byte y)
+    {
+        write((uint32_t)a, (uint32_t)y, sizeof(byte));
+    }
+    inline void writeChar(uint32_t a, char c)
+    {
+        write((uint32_t)a, (uint32_t)c, sizeof(char));
+    }
+    inline void writeUnsignedChar(uint32_t a, unsigned char uc)
+    {
+        write((uint32_t)a, (uint32_t)uc, sizeof(unsigned char));
+    }
+    inline void writeShort(uint32_t a, short l)
+    {
+        write((uint32_t)a, (uint32_t)l, sizeof(short));
+    }
+    inline void writeLong(uint32_t a, long l)
+    {
+        write((uint32_t)a, (uint32_t)l, sizeof(long));
+    }
+    inline void writeUnsignedLong(uint32_t a, unsigned long ul)
+    {
+        write((uint32_t)a, (uint32_t)ul, sizeof(unsigned long));
+    }
+    inline void writeInt(uint32_t a, unsigned int i)
+    {
+        write((uint32_t)a, (uint32_t)i, sizeof(unsigned int));
+    }
+    inline void writeUnsignedInt(uint32_t a, unsigned int ui)
+    {
+        return write((uint32_t)a, (uint32_t)ui, sizeof(unsigned int));
+    }
+    inline void writeWord(uint32_t a, word ui)
+    {
+        return write((uint32_t)a, (uint32_t)ui, sizeof(word));
+    }
+    inline void writeDouble(uint32_t a, double d)
+    {
+        mkconv.dval = d;
+        write((uint32_t)a, (uint8_t *)mkconv.bval, sizeof(double));
+    }
+    inline void writeFloat(uint32_t a, float f)
+    {
+        mkconv.fval = f;
+        write((uint32_t)a, (uint8_t *)mkconv.bval, sizeof(float));
+    }
+    /* ------------------------ Read --------------------- */
+
+    inline bool readBool(uint32_t a)
+    {
+        return (bool)read((uint32_t)a, sizeof(bool));
+    }
+    inline void readCharArray(uint32_t a, char c[], uint8_t i)
+    {
+        read((uint32_t)a, (uint8_t *)c, (uint8_t)i);
+    }
+    inline void readByteArray(uint32_t a, uint8_t y[], uint8_t i)
+    {
+        read((uint32_t)a, (uint8_t *)y, (uint8_t)i);
+    }
+    inline byte readByte(uint32_t a)
+    {
+        return (byte)read((uint32_t)a, sizeof(byte));
+    }
+    inline char readChar(uint32_t a)
+    {
+        return (char)read((uint32_t)a, sizeof(char));
+    }
+    inline unsigned char readUnsignedChar(uint32_t a)
+    {
+        return (unsigned char)read((uint32_t)a, sizeof(unsigned char));
+    }
+    inline short readShort(uint32_t a)
+    {
+        return (short)read((uint32_t)a, sizeof(short));
+    }
+    inline long readLong(uint32_t a)
+    {
+        return (long)read((uint32_t)a, sizeof(long));
+    }
+    inline unsigned long readUnsignedLong(uint32_t a)
+    {
+        return (unsigned long)read((uint32_t)a, sizeof(unsigned long));
+    }
+    inline int readInt(uint32_t a)
+    {
+        return (int)read((uint32_t)a, sizeof(unsigned int));
+    }
+    inline unsigned int readUnsignedInt(uint32_t a)
+    {
+        return (unsigned int)read((uint32_t)a, sizeof(unsigned int));
+    }
+    inline word readword(uint32_t a)
+    {
+        return (word)read((uint32_t)a, sizeof(word));
+    }
+    inline double readDouble(uint32_t a)
+    {
+        read((uint32_t)a, (uint8_t *)mkconv.bval, sizeof(double));
+        return mkconv.dval;
+    }
+    inline float readFloat(uint32_t a)
+    {
+        read((uint32_t)a, (uint8_t *)mkconv.bval, sizeof(float));
+        return mkconv.fval;
+    }
+
+protected:
+private:
+    TwoWire *ATWire; // TwoWire library isntance
+    uint8_t ATDevAdr;
+    union conv
+    {
+        float fval;
+        double dval;
+        uint8_t bval[sizeof(double)];
+    } mkconv;
 };
 
 #endif
